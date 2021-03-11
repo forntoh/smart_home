bf3c015de3c17070d512ae3099e6df20bcf1abdb
forntoh <thomasforntoh@gmail.com>
2021-03-11 17:22:32 +0100

    qdded ,ore colors

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,20 +4,29 @@ const double defaultPadding = 20.0;
 Color primaryColor = Color(0xFF4B29AC);
 Color accentColor = Color(0xFFF85C47);
 
-Color bgGradientBotomStart = Color(0xFF07CCD5);
-Color bgGradientBottom = Color(0xFF8B66FF);
-Color bgGradientBotomEnd = Color(0xFFF85C47);
+Color bgGradientBotomStart = Color(0xFF00C4B6);
+Color bgGradientBotomStartQ = Color(0xFF0697E1);
+Color bgGradientBottomMiddle = Color(0xFF7E5EF9);
+Color bgGradientBotomEndQ = Color(0xFFF6239C);
+Color bgGradientBotomEnd = Color(0xFFF85D48);
+
 
 Color getColor(double progress) {
-  var beginRange = mapToRange(progress, 0, 1, 0, 0.5);
-  var endRange = mapToRange(progress, 0, 1, 0.5, 1);
+  var first = mapToRange(progress, 0, 1, 0, 0.25);
+  var second = mapToRange(progress, 0, 1, 0.25, 0.5);
+  var third = mapToRange(progress, 0, 1, 0.5, 0.75);
+  var forth = mapToRange(progress, 0, 1, 0.75, 1);
 
-  if (progress >= 0 && progress <= 0.5)
-    return Color.lerp(bgGradientBotomStart, bgGradientBottom, beginRange);
-  else if (progress > 0.5 && progress <= 1)
-    return Color.lerp(bgGradientBottom, bgGradientBotomEnd, endRange);
+  if (progress >= 0 && progress <= 0.25)
+    return Color.lerp(bgGradientBotomStart, bgGradientBotomStartQ, first);
+  else if (progress > 0.25 && progress <= 0.5)
+    return Color.lerp(bgGradientBotomStartQ, bgGradientBottomMiddle, second);
+  else if (progress > 0.5 && progress <= 0.75)
+    return Color.lerp(bgGradientBottomMiddle, bgGradientBotomEndQ, third);
+  else if (progress > 0.75 && progress <= 1)
+    return Color.lerp(bgGradientBotomEndQ, bgGradientBotomEnd, forth);
   else
-    return bgGradientBottom;
+    return bgGradientBottomMiddle;
 }
 
 double mapToRange(
